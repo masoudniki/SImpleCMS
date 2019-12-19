7eae9cbff8e7eb942243ea6f643a8e435d93d222
MasoudNiki <masoud.niki79@gmail.com>
2019-12-19 20:11:46 +0330

    creating upload form and check user is logged in

--- admin/Upload.php
+++ admin/Upload.php
@@ -1,4 +1,12 @@
 <?php include("includes/header.php"); ?>
+<?php
+
+    if(!$session->is_signed_in())
+    {
+        redirect("login.php");
+    }
+    
+?>
 
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -29,14 +37,24 @@
                             Upload
                             <small>Subheading</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                        <div class="col-md-6">
+                            <form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data" >
+                            
+                                <div class="form-group">
+                                
+                                    <input type="text" name="title" id="" class="form-control">
+                                
+                                </div>
+                                <div class="form-group">
+                                
+                                    <input type="file" name="file_upload" id="" class="form-control">
+                                
+                                </div>
+                                <input type="submit" name="submit" value="submit">
+                            
+                            
+                            </form>
+                      </div>
                     </div>
                 </div>
                 <!-- /.row -->
