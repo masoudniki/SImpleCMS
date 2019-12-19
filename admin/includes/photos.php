fcd2e4a64fc7b7b0d63fa93f1e4ef68eac95d205
MasoudNiki <masoud.niki79@gmail.com>
2019-12-19 20:44:32 +0330

    silly job :|

--- admin/includes/photos.php
+++ admin/includes/photos.php
@@ -19,7 +19,6 @@
             UPLOAD_ERR_FORM_SIZE    =>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
             UPLOAD_ERR_PARTIAL      =>"The uploaded file was only partially uploaded.",
             UPLOAD_ERR_NO_FILE      =>"No file was uploaded. ",
-            5=>null,
             UPLOAD_ERR_NO_TMP_DIR   =>"Missing a temporary folder. Introduced in PHP 5.0.3. ",
             UPLOAD_ERR_CANT_WRITE   =>"Failed to write file to disk. Introduced in PHP 5.1.0. ",
             UPLOAD_ERR_EXTENSION    =>"A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help. Introduced in PHP 5.2.0. "
