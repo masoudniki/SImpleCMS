9a12d2a1aebaba78e3c173bc2cc5718e2aded57b
MasoudNiki <masoud.niki79@gmail.com>
2019-12-19 18:52:57 +0330

    define some constand for path

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,7 @@
 <?php
+    defined('DS') ? null : define("DS",DIRECTORY_SEPARATOR);
+    define("SITE_ROOT","C:".DS."xampp".DS."htdocs".DS."Gallery");
+    defined('INCLUDES_PATH') ? null :define("INCLUDES_PATH",SITE_ROOT.DS.'admin'.DS."includes");
     require_once("function.php");
     require_once("Newconfig.php");
     require_once("Database.php");
