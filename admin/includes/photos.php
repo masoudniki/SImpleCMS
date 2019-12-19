97425f913621f905b126b808bcf6367a42f490a9
MasoudNiki <masoud.niki79@gmail.com>
2019-12-19 15:31:31 +0330

    adding photo table field and propertie to the class

--- admin/includes/photos.php
+++ admin/includes/photos.php
@@ -1,7 +1,14 @@
 <?php
     class Photo extends db_object{
 
-
+        protected static $db_table="photos";//name of the user table 
+        protected static $db_table_fields=['photo_id','title','description','filename','type','size'];
+        public $ID;
+        public $title;
+        public $description;
+        public $type;
+        public $size;
+        
 
 
 
