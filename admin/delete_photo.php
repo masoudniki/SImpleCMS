cf2d389937e1636a7601ffacbd80083f248244f9
MasoudNiki <masoud.niki79@gmail.com>
2019-12-20 19:14:38 +0330

    creating phot delete page

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -13,8 +13,21 @@
   {
     redirect("../photos.php");
   }
-  
+
   $photo=photo::find_by_id($_GET['id']);
+
+  if($photo)
+  {
+
+    $photo->delete_photo();
+    redirect("../photos.php");
+
+  }
+  else{
+
+      $message="soryy the photo ID does not exist";
+      redirect("../photos.php");
+  }
 
 
 
