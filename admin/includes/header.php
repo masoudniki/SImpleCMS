12b0345b135e414bede605af76d36f0c7b5ea7a0
MasoudNiki <masoud.niki79@gmail.com>
2019-12-22 17:59:37 +0330

    smalling the picture in photo page admin

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -31,6 +31,15 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+        img
+        {
+            max-width: 200px;
+            max-height: 125px;
+        }
+    
+    
+    </style>
 
 </head>
 
