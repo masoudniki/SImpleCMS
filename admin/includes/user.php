de276e033420693902124292997832c9ba6c5163
MasoudNiki <masoud.niki79@gmail.com>
2019-12-26 22:17:14 +0330

    adding custom error handler

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -13,6 +13,7 @@
         public $user_image;
         public $up_dir="Avatar";
         public $place_holder="https://via.placeholder.com/150x150?text=Image";
+        public $custom_err=array();
         
         public function image_path_and_placeholder(){
             return (empty($this->user_image)?$this->place_holder:$this->up_dir.DS.$this->user_image);
@@ -47,6 +48,81 @@
 
 
         }
+        public function set_file($file)
+        {
+            if(empty($file) || !$file || !is_array($file))
+            {
+                $this->custom_err[]="There was no file uploded here";
+                return false;
+
+            }
+            elseif($file['error']!=0)
+            {
+
+                $this->custom_err[]=$this->Erorr_Upload_file[$file['error']];
+                return false;
+
+            }
+            else
+            {
+                $this->user_image   =basename($file['name']);
+                $this->tmp_path   =$file['tmp_name'];
+                $this->type       =$file['type'];
+                $this->size       =$file['size'];
+
+            }
+
+
+
+
+
+        }
+        public function save_user_and_image()
+        {
+
+            if($this->ID)
+            {
+
+                $this->Update();
+
+            }
+            else{
+
+                if(!empty($this->custom_err))
+                {
+                    return false;
+                }
+                if(empty($this->user_image) || empty($this->tmp_path) )
+                {
+                    $this->error[]="the file was not available";
+                    return false;
+                }
+
+                $target_path=SITE_ROOT.DS."admin".DS.$this->up_dir.DS.$this->user_image;
+
+                if(file_exists($target_path))
+                {
+                    $this->custom_err[]="the file {$this->user_image} already exist";
+                    return false;
+                }
+
+                if(move_uploaded_file($this->tmp_path,$target_path))
+                {
+                    if($this->create())
+                    {
+                        unset($this->tmp_path);
+                        return true;
+                    }
+                }
+
+                else{
+
+                    $this->custom_err[]="the file directory probably does not have permisson";
+                    return false;
+                }
+
+
+            }
 
     
 
@@ -67,7 +143,7 @@
 
 
 
-
+    }
 
 
 
