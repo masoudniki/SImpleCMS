35d6b5aeb10f047949bb4d9684fee68ffc842fe1
MasoudNiki <masoud.niki79@gmail.com>
2019-12-27 19:29:06 +0330

    fixing update mehtod to retrun state of excute sql

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -5,6 +5,21 @@ class db_object{
 
 
     protected static $db_table="users";
+
+    public $custom_err=array();
+    public   $Erorr_Upload_file=
+    [
+        UPLOAD_ERR_OK           =>"There is no error, the file uploaded with success.",
+        UPLOAD_ERR_INI_SIZE     =>"The uploaded file exceeds the upload_max_filesize directive in php.ini.",
+        UPLOAD_ERR_FORM_SIZE    =>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+        UPLOAD_ERR_PARTIAL      =>"The uploaded file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE      =>"No file was uploaded. ",
+        UPLOAD_ERR_NO_TMP_DIR   =>"Missing a temporary folder. Introduced in PHP 5.0.3. ",
+        UPLOAD_ERR_CANT_WRITE   =>"Failed to write file to disk. Introduced in PHP 5.1.0. ",
+        UPLOAD_ERR_EXTENSION    =>"A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help. Introduced in PHP 5.2.0. "
+    ];
+
+
     public static function find_all()
     {
 
@@ -98,9 +113,9 @@ class db_object{
             $sql.=" Where  ".static::$search_table." =".$database->escape_string($this->{static::$search_table});
            
 
-            $database->query($sql);
-
-            return (mysqli_affected_rows($database->connection) ==1) ? true : false;
+            
+            
+            return ($database->query($sql)) ? true : false;
 
 
 
