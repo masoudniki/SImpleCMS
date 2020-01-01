2d97048b33ddce7b5b7cd645abf2714115c66a14
MasoudNiki <masoud.niki79@gmail.com>
2020-01-01 20:41:38 +0330

    creatin photo page to show pic

--- photo.php
+++ photo.php
@@ -1,3 +1,45 @@
+<?php require_once("admin/includes/init.php")?>
+
+<?php
+if(empty($_GET['id']))
+{
+
+    redirect("index.php");
+
+
+
+}
+$photo=photo::find_by_id($_GET['id']);
+
+if(isset($_POST['submit']))
+{
+    file_put_contents("hello.txt","hello");
+    $author=trim($_POST['author']);
+    $body=trim($_POST['body']);
+    file_put_contents("hello.txt",json_encode($_POST));
+    
+    $comment=Comment::create_comment($photo->photo_id,$author,$body);
+    
+
+    if($comment && $comment->save())
+    {
+        redirect("photo.php?id={$photo->photo_id}");
+    }
+
+    
+}
+$comments=comment::find_the_comment($photo->photo_id);
+echo "<pre>";
+var_dump($comments);
+echo "</pre>";
+
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -86,7 +128,7 @@
                 <hr>
 
                 <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="<?php echo "admin".DS.$photo->GetPicPath(); ?>" alt="">
 
                 <hr>
 
@@ -104,11 +146,15 @@
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form method="post" role="form">
+                    <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" name="author" id="" class="form-control">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <textarea name="body" class="form-control" rows="3"></textarea>
+                        </div>
+                        <input type="submit" value="submit" name="submit" class="btn btn-primary">
                     </form>
                 </div>
 
@@ -117,29 +163,33 @@
                 <!-- Posted Comments -->
 
                 <!-- Comment -->
+                <?php foreach($comments as $comment):?>
                 <div class="media">
+                    
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
+                        <h4 class="media-heading"><?php echo $comment->author?>
                             <small>August 25, 2014 at 9:30 PM</small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <?php echo $comment->body; ?>
                     </div>
+                     
                 </div>
+                <?php endforeach;?>
 
                 <!-- Comment -->
-                <div class="media">
+                <!-- <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
+                        <h4 class="media-heading">fsdfsaf
                             <small>August 25, 2014 at 9:30 PM</small>
                         </h4>
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
+                        <!-- Nested Comment 
                         <div class="media">
                             <a class="pull-left" href="#">
                                 <img class="media-object" src="http://placehold.it/64x64" alt="">
@@ -150,12 +200,12 @@
                                 </h4>
                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                             </div>
-                        </div>
-                        <!-- End Nested Comment -->
+                        </div> -->
+                        <!-- End Nested Comment
                     </div>
-                </div>
+                </div> -->
 
-            </div>
+            </div> 
 
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
