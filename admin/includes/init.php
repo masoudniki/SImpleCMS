f2f911bf3c99d5feda8316672a2e3d270589f505
MasoudNiki <masoud.niki79@gmail.com>
2020-01-01 20:42:44 +0330

    fixing db_object didnt add to project

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -2,13 +2,14 @@
     defined('DS') ? null : define("DS",DIRECTORY_SEPARATOR);
     define("SITE_ROOT","C:".DS."xampp".DS."htdocs".DS."Gallery");
     defined('INCLUDES_PATH') ? null :define("INCLUDES_PATH",SITE_ROOT.DS.'admin'.DS."includes");
+    require_once("db_object.php");
     require_once("function.php");
     require_once("Newconfig.php");
     require_once("Database.php");
     require_once("user.php");
     require_once("session.php");
-    require_once("db_object.php");
     require_once("photos.php");
+    require_once("comment.php");
 
 
 
