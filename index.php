d389b863f0d0780e1b24b75ce2ffc55ff317e2ef
MasoudNiki <masoud.niki79@gmail.com>
2020-01-03 21:20:22 +0330

    shoing picture to users

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <?php include("includes/header.php"); ?>
+<?php
+    $photos=photo::find_all();
+    
 
+
+?>
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
+                <div class="tuhmbnails row">
+                  <?php foreach($photos as $photo):?>
+                
+                
+                    <div class="col-xs-6 col-md-3">
+                    
+                    <a class="thumbnail" href="photo.php?id=<?php echo $photo->photo_id;?>">
+                        <img  class=" img-responsive home_page_photo" src="admin/<?php echo $photo->GetPicPath();?>" alt="">
+                    
+                    </a>
+                    
+                    
+                    </div>
 
-    
-            
-          
+                
+                
+                   
+                <?php endforeach;?> 
+                </div>  
          
 
             </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
